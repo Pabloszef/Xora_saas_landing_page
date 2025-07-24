d191cca6e9488b7ed45e443b7ce29e1c0c0bc6f3
unknown <pabloszef2115@gmail.com>
2025-07-24 19:33:00 +0200

    Implement Features section, new navbar animation, fix scrolling animation and started the Pricing section

---
+++ src/sections/Pricing.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Element } from "react-scroll";
+
+const Pricing = () => {
+  return (
+    <section>
+      <Element name="pricing">
+        <div className="container">
+          <div className="max-w-960 border-2 border-red-500">
+            <h3>Flexible pricing for teams of all sizes</h3>
+          </div>
+        </div>
+      </Element>
+    </section>
+  );
+};
+export default Pricing;
