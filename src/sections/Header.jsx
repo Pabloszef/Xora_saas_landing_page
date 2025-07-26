d10aabbdff45b97c18cfc6f2024fc3ade419150d
unknown <pabloszef2115@gmail.com>
2025-07-26 18:14:59 +0200

    Implement Testimonials section and custom item

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -21,13 +21,12 @@ const Header = () => {
   const NavLink = ({ title }) => (
     <LinkScroll
       onClick={() => setIsOpen(false)}
-      activeClass="nav-active"
+      to={title}
+      offset={-100}
       spy
       smooth
-      offset={-100}
-      to={title}
-      className="base-bold text-p4 uppercase transition-colors duration-500 cursor-pointer hover:text-p1
-    max-lg:my-4 max-lg:h5"
+      activeClass="nav-active"
+      className="base-bold text-p4 uppercase transition-colors duration-500 cursor-pointer hover:text-p1 max-lg:my-4 max-lg:h5"
     >
       {title}
     </LinkScroll>
@@ -37,7 +36,7 @@ const Header = () => {
     <header
       className={clsx(
         "fixed top-0 left-0 z-50 w-full py-10 transition-all duration-500 max-lg:py-4",
-        hasScrolled && "py-2 bg-black-100 backdrop-blur-[8px] ",
+        hasScrolled && "py-2 bg-black-100 backdrop-blur-[8px]",
       )}
     >
       <div className="container flex h-14 items-center max-lg:px-5">
@@ -47,15 +46,11 @@ const Header = () => {
 
         <div
           className={clsx(
-            "w-full max-lg:fixed max-lg:top-0 max-lg:left-0 max-lg:w-full\n" +
-              "        max-lg:bg-s2 max-lg:opacity-0",
+            "w-full max-lg:fixed max-lg:top-0 max-lg:left-0 max-lg:w-full max-lg:bg-s2 max-lg:opacity-0",
             isOpen ? "max-lg:opacity-100" : "max-lg:pointer-events-none",
           )}
         >
-          <div
-            className=" max-lg:relative max-lg:flex max-lg:flex-col max-lg:min-h-screen
-                max-lg:p-6 max-lg:overflow-hidden sidebar-before max-md:px-4"
-          >
+          <div className="max-lg:relative max-lg:flex max-lg:flex-col max-lg:min-h-screen max-lg:p-6 max-lg:overflow-hidden sidebar-before max-md:px-4">
             <nav className="max-lg:relative max-lg:z-2 max-lg:my-auto">
               <ul className="flex max-lg:block max-lg:px-12">
                 <li className="nav-li">
@@ -91,10 +86,7 @@ const Header = () => {
               </ul>
             </nav>
 
-            <div
-              className="lg:hidden block absolute top-1/2 left-0 w-[960px]
-            h-[380px] translate-x-[-290px] -translate-y-1/2 rotate-90"
-            >
+            <div className="lg:hidden block absolute top-1/2 left-0 w-[960px] h-[380px] translate-x-[-290px] -translate-y-1/2 rotate-90">
               <img
                 src="/images/bg-outlines.svg"
                 width={960}
@@ -114,8 +106,7 @@ const Header = () => {
         </div>
 
         <button
-          className="lg:hidden z-2 size-10 border-2 border-s4/25 rounded-full
-        flex justify-center items-center"
+          className="lg:hidden z-2 size-10 border-2 border-s4/25 rounded-full flex justify-center items-center"
           onClick={() => setIsOpen((prevState) => !prevState)}
         >
           <img
@@ -128,4 +119,5 @@ const Header = () => {
     </header>
   );
 };
+
 export default Header;
